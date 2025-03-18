b9987195a67968c271e5ab38dc21565a2f812bf2
HyeonWoo <rmxhak2@naver.com>
2025-03-18 19:29:42 +0900

    feat: Palindrome page component complete
    - palindrome 페이지 컴포넌트 완성
    - Componet 추상클래스 수정
    	- state를 읽기전용으로 수정

--- src/pages/palindrome/Palindrome.ts
+++ src/pages/palindrome/Palindrome.ts
@@ -1,8 +1,59 @@
+import st from './palindrome.module.scss';
+import { html } from '@/utils/template';
 import Component from '../../common/Component';
+import { EventHandler } from '@/common/eventHolder';
 
-class Palindrome extends Component {
+type PalindromeState = {
+  value: string;
+  isPalindrom: null | boolean;
+};
+
+class Palindrome extends Component<PalindromeState> {
+  constructor() {
+    super({
+      value: '',
+      isPalindrom: null,
+    });
+  }
+
   render() {
-    return `<div>여기는 Palindrome입니다.</div>`;
+    return html`
+      <section class="${st.page}">
+        <h1 class="${st.title}">Is Palindrome?</h1>
+        <section>
+          <article class="${st.container}">
+            <form class="${st.submit}">
+              <input class="${st.input}" type="text" placeholder="Enter a word to check" />
+              <button class="${st.button}">Check</button>
+            </form>
+            <span class="${st.result} ${this.state.value !== '' ? '' : st.hidden}"
+              >"${this.state.value}" is a ${this.state.isPalindrom ? '' : 'not'} palindrome</span
+            >
+          </article>
+        </section>
+      </section>
+    `;
+  }
+
+  isPalindrome(str: string): boolean {
+    const normalizedStr = str.toLowerCase().replace(/[\w_\s]/g, '');
+    const reverse = normalizedStr.split('').reverse().join('');
+
+    return normalizedStr === reverse;
+  }
+
+  handleSubmit(e: Event) {
+    e.preventDefault();
+
+    const target = e.target as HTMLFormElement;
+    const value = (target.querySelector(`.${st.input}`) as HTMLInputElement).value;
+    const isPalindrorm = this.isPalindrome(value);
+
+    this.setState({ isPalindrom: isPalindrorm, value });
+  }
+
+  protected addEventListeners(): EventHandler[] {
+    return [{ type: 'submit', selector: `.${st.submit}`, handler: this.handleSubmit.bind(this) }];
   }
 }
 
